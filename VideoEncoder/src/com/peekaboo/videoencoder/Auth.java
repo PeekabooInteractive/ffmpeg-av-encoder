d65801ec48cafb75ec993191c207fc9849aa3245
ocrespo <ocresposalazar@gmail.com>
2014-07-24 13:57:13 +0100

    Initial demo working of youtube

--- VideoEncoder/src/com/peekaboo/videoencoder/Auth.java
+++ VideoEncoder/src/com/peekaboo/videoencoder/Auth.java
@@ -1,4 +1,4 @@
-package co.uk.peekaboo.videoencoder;
+package com.peekaboo.videoencoder;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +11,7 @@ import android.util.Log;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.StoredCredential;
+import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
@@ -18,9 +19,14 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.DataStore;
 import com.google.api.client.util.store.FileDataStoreFactory;
+
+
+
+
 
 /**
  * Shared class used by every sample. Contains methods for authorizing a user and caching credentials.
@@ -31,16 +37,22 @@ public class Auth {
      * Define a global instance of the HTTP transport.
      */
     public static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    
+    public static final HttpTransport HTTP_TRANSPORT_DEFAULT = AndroidHttp.newCompatibleTransport();
 
     /**
      * Define a global instance of the JSON factory.
      */
     public static final JsonFactory JSON_FACTORY = new JacksonFactory();
+    
+    public static final JsonFactory JSON_FACTORY_DEFAULT = GsonFactory.getDefaultInstance();
 
     /**
      * This is the directory that will be used under the user's home directory where OAuth tokens will be stored.
      */
     private static final String CREDENTIALS_DIRECTORY = ".oauth-credentials";
+    
+    
 
     /**
      * Authorizes the installed application to access user's protected data.
@@ -48,7 +60,7 @@ public class Auth {
      * @param scopes              list of scopes needed to run youtube upload.
      * @param credentialDatastore name of the credential datastore to cache OAuth tokens
      */
-    public static Credential authorize(List<String> scopes, String credentialDatastore,InputStream stream ) throws IOException {
+    public static Credential authorize(List<String> scopes, String credentialDatastore,InputStream stream, String path ) throws IOException {
     	Log.e("VideoUploader", "DONE1");
         // Load client secrets.
     	//InputStream aux = Auth.class.getClassLoader().getResourceAsStream("/client_secrets.json");
@@ -70,17 +82,20 @@ public class Auth {
         }
 
         // This creates the credentials datastore at ~/.oauth-credentials/${credentialDatastore}
-        FileDataStoreFactory fileDataStoreFactory = new FileDataStoreFactory(new File(System.getProperty("user.home") + "/" + CREDENTIALS_DIRECTORY));
+        //FileDataStoreFactory fileDataStoreFactory = new FileDataStoreFactory(new File(System.getProperty("user.home") + "/" + CREDENTIALS_DIRECTORY));
+        FileDataStoreFactory fileDataStoreFactory = new FileDataStoreFactory(new File(path + "/" + CREDENTIALS_DIRECTORY));
+        Log.e("VideoUploader", "DONE4");
         DataStore<StoredCredential> datastore = fileDataStoreFactory.getDataStore(credentialDatastore);
+        Log.e("VideoUploader", "DONE5");
 
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, scopes).setCredentialDataStore(datastore)
-                .build();
-
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, scopes).setCredentialDataStore(datastore).build();
+        Log.e("VideoUploader", "DONE6");
         // Build the local server and bind it to port 8080
         LocalServerReceiver localReceiver = new LocalServerReceiver.Builder().setPort(8080).build();
-
+        Log.e("VideoUploader", "DONE7");
         // Authorize.
+        
+        
         return new AuthorizationCodeInstalledApp(flow, localReceiver).authorize("user");
     }
 }
--- VideoEncoder/src/com/peekaboo/videoencoder/VideoUploader.java
+++ VideoEncoder/src/com/peekaboo/videoencoder/VideoUploader.java
@@ -1,25 +1,38 @@
-package co.uk.peekaboo.videoencoder;
+package com.peekaboo.videoencoder;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Arrays;
 import java.util.List;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Notification;
+import android.app.Notification.Builder;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.api.client.auth.oauth2.Credential;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.common.Scopes;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.googleapis.media.MediaHttpUploader;
 import com.google.api.client.googleapis.media.MediaHttpUploaderProgressListener;
 import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.util.ExponentialBackOff;
 import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.YouTubeScopes;
 import com.google.api.services.youtube.model.Video;
 import com.google.api.services.youtube.model.VideoSnippet;
 import com.google.api.services.youtube.model.VideoStatus;
-import com.google.common.collect.Lists;
 import com.unity3d.player.UnityPlayerActivity;
 //import com.google.api.services.samples.youtube.cmdline.Auth;
 
@@ -42,41 +55,89 @@ public class VideoUploader extends UnityPlayerActivity {
     private static final String SAMPLE_VIDEO_FILENAME = "sample-video.mp4";
     
     public static Context context;
+    
+    public static Bundle bundle;
+    
+    public static VideoUploader activity;
+    
+    private static GoogleAccountCredential credential;
+    
+    public static final String KEY = "231250131118-4r3gg0j71q2bp71jsc4605sh1m290knn.apps.googleusercontent.com";
 
+    public static final String[] SCOPES = {Scopes.PLUS_ME,YouTubeScopes.YOUTUBE_UPLOAD,YouTubeScopes.YOUTUBE_READONLY,YouTubeScopes.YOUTUBE};
+    
+    private static SharedPreferences settings;
 
     protected void onCreate(Bundle savedInstanceState) {
 
 		// call UnityPlayerActivity.onCreate()
 		super.onCreate(savedInstanceState);
-		
+		settings =  getPreferences(Context.MODE_PRIVATE);
 		context = this;
+		activity = this;
+		bundle = savedInstanceState;
 		// print debug message to logcat
 		Log.e("VideoUploader", "onCreate called!");
     }
 
     public void onBackPressed(){
-    	
+    	//UploadVideo("");
 	    // instead of calling UnityPlayerActivity.onBackPressed() we just ignore the back button event
 	    // super.onBackPressed();
     }
   
-    public static void  UploadVideo(){
+    public static void  UploadVideo(String path){
     	Log.e("VideoUploader", "EXECUTE!!!!!!!!!!");
     	
     	
         // This OAuth 2.0 access scope allows an application to upload files
         // to the authenticated user's YouTube channel, but doesn't allow
         // other types of access.
-        List<String> scopes = Lists.newArrayList("https://www.googleapis.com/auth/youtube.upload");
-
+        //List<String> scopes = Lists.newArrayList("https://www.googleapis.com/auth/youtube.upload");
+    	Account mAccount;
         try {
+        	mAccount = AccountManager.get(context).getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE)[0];
         	
-        	InputStream input = context.getAssets().open("client_secrets.json");
+        	/*String tokens = GoogleAuthUtil.getToken(activity,mAccount.name, "oauth2:client_id:"+ KEY +":api_scope:"+Scopes.PLUS_ME+" "+Scopes.GAMES+" "+Scopes.PROFILE+" "+Scopes.PLUS_LOGIN+" "+Scopes.APP_STATE+" https://www.googleapis.com/auth/plus.login "+
+        			Scopes.CLOUD_SAVE+" "+Scopes.DRIVE_APPFOLDER+" "+YouTubeScopes.YOUTUBEPARTNER+" "+YouTubeScopes.YOUTUBEPARTNER_CHANNEL_AUDIT+" "
+        			+YouTubeScopes.YOUTUBE_READONLY+" "+YouTubeScopes.YOUTUBE_UPLOAD+" "+YouTubeScopes.YOUTUBE+" "+YouTubeScopes.YOUTUBEPARTNER,bundle);*/
+        	
+        	/*String tokens = GoogleAuthUtil.getTokenWithNotification(activity,mAccount.name, "oauth2:"+Scopes.PLUS_ME+" "+Scopes.GAMES+" "+Scopes.PROFILE+" "+Scopes.PLUS_LOGIN
+        			+" "+YouTubeScopes.YOUTUBE_READONLY+" "+YouTubeScopes.YOUTUBE_UPLOAD+" "+YouTubeScopes.YOUTUBE,null);*/
+        	
+        	Log.e("VideoUploader", "Ready credentials");     
+        	
+        	//credential = GoogleAccountCredential.usingAudience(activity, "audience:server:client_id:"+KEY+":api_scope:"+Scopes.PLUS_ME+" "+YouTubeScopes.YOUTUBE_READONLY+" "+YouTubeScopes.YOUTUBE_UPLOAD);
+        
+        	credential = GoogleAccountCredential. usingOAuth2(context,Arrays.asList(SCOPES));// Collections.singleton(YouTubeScopes.YOUTUBE_UPLOAD));
+        	
+        	//SharedPreferences settings = context.getSharedPreferences("", Context.MODE_PRIVATE);
+        	 //SharedPreferences settings = context.getSharedPreferences(Context.MODE_PRIVATE,0);
+        	
+        	
+        	//SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+    		//String mChosenAccountName = sp.getString("accountName", null);
+        	
+        	credential.setSelectedAccountName(mAccount.name);
+        	
+        	credential.getToken();
+        	
+        	Log.e("VideoUploader", "settings "+settings.getString("accountName", null));
+        	Log.e("VideoUploader"," Account "+mAccount.name);
+    
+    	    credential.setBackOff(new ExponentialBackOff());
+        	
+        	
+        	Log.e("VideoUploader", "youtube credentials");
+    		youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT_DEFAULT, Auth.JSON_FACTORY_DEFAULT, credential).setApplicationName("Video-trial").build();
+        	        	
+        	//InputStream input = context.getAssets().open("client_secrets.json");
         	// Authorize the request.
-            Credential credential = Auth.authorize(scopes, "uploadvideo",input);
+            //Credential credential = Auth.authorize(scopes, "uploadvideo",input,path);
 
+            
             // This object is used to make YouTube Data API requests.
-            youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, credential).setApplicationName("VideoUploader").build();
+            //youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, credential).setApplicationName("VideoUploader").build();
 
             Log.e("VideoUploader","Uploading: " + SAMPLE_VIDEO_FILENAME);
 
@@ -96,9 +157,9 @@ public class VideoUploader extends UnityPlayerActivity {
             // description for test purposes so that you can easily upload
             // multiple files. You should remove this code from your project
             // and use your own standard names instead.
-            Calendar cal = Calendar.getInstance();
-            snippet.setTitle("Test Upload via Java on " + cal.getTime());
-            snippet.setDescription("Video uploaded via YouTube Data API V3 using the Java library " + "on " + cal.getTime());
+           // Calendar cal = Calendar.getInstance();
+            snippet.setTitle("Test Upload via Java on ");
+            snippet.setDescription("Video uploaded via YouTube Data API V3 using the Java library");
 
             // Set the keyword tags that you want to associate with the video.
             List<String> tags = new ArrayList<String>();
@@ -112,7 +173,7 @@ public class VideoUploader extends UnityPlayerActivity {
             // Add the completed snippet object to the video resource.
             videoObjectDefiningMetadata.setSnippet(snippet);
 
-            InputStreamContent mediaContent = new InputStreamContent(VIDEO_FILE_FORMAT,VideoUploader.class.getResourceAsStream(SAMPLE_VIDEO_FILENAME));
+            InputStreamContent mediaContent = new InputStreamContent(VIDEO_FILE_FORMAT,context.getAssets().open(SAMPLE_VIDEO_FILENAME));
 
             // Insert the video. The command sends three arguments. The first
             // specifies which information the API request is setting and which
@@ -175,6 +236,14 @@ public class VideoUploader extends UnityPlayerActivity {
         } catch (IOException e) {
             System.err.println("IOException: " + e.getMessage());
             e.printStackTrace();
+        }catch (GooglePlayServicesAvailabilityException playEx) {
+        	System.err.println("GooglePlayServicesAvailabilityException authentication exception: " + playEx.getMessage());
+        }catch (UserRecoverableAuthException recoverableException) {
+        	activity.startActivityForResult(recoverableException.getIntent(), 0);        	
+        	System.err.println("UserRecoverableAuthException authentication exception: " + recoverableException.getMessage());
+        } catch (GoogleAuthException authEx) {
+            // This is likely unrecoverable.
+        	 System.err.println("Unrecoverable authentication exception: " + authEx.getMessage());
         } catch (Throwable t) {
             System.err.println("Throwable: " + t.getMessage());
             t.printStackTrace();
